8fe10834ae9311127b0fc1ee59b88c0f902d4f02
Artem Koziar <koziar.artem@gmail.com>
2020-11-09 18:54:52 +0200

    Configure ESLint with Prettier

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 (function (window) {
-  console.log(
-      'Hello, console!'
-  );
+  console.log('Hello, console!');
   var a = 1;
   const b = 2;
-  let s1 = "123";
-      let s2 = '123';
+  let s1 = '123';
+  let s2 = '123';
 
-      const f1 = () => null;
+  const f1 = () => null;
 
-      const f2 = function (){};
+  const f2 = function () {};
 
-      function f3() {}
+  function f3() {}
 
-      f3();
+  f3();
 
-}(window));
+  const THE_SOME_MOST_VERY_IMPORTANT_CONST_EVER_IN_THE_WORLD =
+    'THE_SOME_MOST_VERY_IMPORTANT_CONST_EVER_IN_THE_WORLD THE_SOME_MOST_VERY_IMPORTANT_CONST_EVER_IN_THE_WORLD';
+
+  console.log(THE_SOME_MOST_VERY_IMPORTANT_CONST_EVER_IN_THE_WORLD);
+})(window);
