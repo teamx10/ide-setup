147c44a81caf0e19d68a108ff410eee8a9b3f3ff
Artem Koziar <koziar.artem@gmail.com>
2020-11-09 18:46:57 +0200

    yarn run eslint --init (w/o ts & react)

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@
   console.log(
       'Hello, console!'
   );
+  var a = 1;
+  const b = 2;
+  let s1 = "123";
+      let s2 = '123';
+
+      const f1 = () => null;
+
+      const f2 = function (){};
+
+      function f3() {}
+
+      f3();
+
 }(window));
